cbb61b1ad7789a9900ca93737e5edf230115efdf
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-09-17 01:51:06 +0200

    Adding some methods to dijkstra tests

--- ralgo/tests/testthat/test_dijkstra.R
+++ ralgo/tests/testthat/test_dijkstra.R
@@ -5,3 +5,7 @@ test_that("dijkstra", {
   expect_error(dijkstra(wiki_graph,99))
 })
 
+test_that("dijkstra", {
+  expect_equivalent(dijkstra(wiki_graph, 1), c(0,7,9,20,20,11))
+  expect_equivalent(dijkstra(wiki_graph, 3), c(9,10,0,11,11,2))
+})
