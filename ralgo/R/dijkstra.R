b15ff7319f2832aa3f384217afb44e37ff37adff
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-09-17 00:30:32 +0200

    Adding init_node arg validation

--- ralgo/R/dijkstra.R
+++ ralgo/R/dijkstra.R
@@ -1,54 +1,56 @@
 #'Dijkstras algorithm
 #'
-#'@param graph is a data.frame contaning the edges in the graph, varibles v1, v2 and w 
+#'@param graph is a data.frame contaning the edges in the graph, varibles v1, v2 and w
 #'@param init_node is a numeric value that exist in the graph data.frame
 #'@details The algoritm takes a graph and an initial node and iteratively calculates the shortest distance between the initial node and every other node in the graph.
 #'@author Yumeng Li, Mattias Karlsson, Ashraf Sarhan
 #'@examples
 #'dijkstra(wiki_graph, 1)
 #'dijkstra(wiki_graph, 3)
-#'@references 
+#'@references
 #'\url{https://en.wikipedia.org/wiki/Dijkstra's_algorithm}
-#'@seealso 
+#'@seealso
 #'\code{\link{euclidean}}
 #'@export
 dijkstra <- function(graph, init_node){
   # initiate unvisited vertex set
   vertexQ = unique(graph$v1)
-  
-  # initiate result table
-  rd = data.frame(matrix(nrow=length(vertexQ), ncol=2))
-  colnames(rd) = c("dist", "prev")
-  rd$dist = Inf
- 
-  # distance from source to source
-  rd[init_node, ]$dist = 0
-  
-  while(length(vertexQ) > 0){
-    for(u in vertexQ){
-      if(rd[u, ]$dist != min(rd[vertexQ, ]$dist)){
-        next
-      }
-      
-      # remove u from unvisited set
-      vertexQ = vertexQ[!vertexQ %in% u]
-      
-      # get neignbors of u that are still in Q
-      ng = graph[graph$v1 == u, ]
-      nv = ng$v2
-      nv = nv[nv %in% vertexQ]
-      
-      # update path of neighbours
-      for(v in nv){
-        alt = rd[u, ]$dist + ng[ng$v2 == v, ]$w
-        if(alt < rd[v, ]$dist){
-          rd[v, ]$dist = alt
-          rd[v, ]$prev = u
+  if (init_node > length(vertexQ)) {
+    stop('init_node must be smaller the biggest node!')
+  } else {
+    # initiate result table
+    rd = data.frame(matrix(nrow=length(vertexQ), ncol=2))
+    colnames(rd) = c("dist", "prev")
+    rd$dist = Inf
+
+    # distance from source to source
+    rd[init_node, ]$dist = 0
+
+    while(length(vertexQ) > 0){
+      for(u in vertexQ){
+        if(rd[u, ]$dist != min(rd[vertexQ, ]$dist)){
+          next
+        }
+
+        # remove u from unvisited set
+        vertexQ = vertexQ[!vertexQ %in% u]
+
+        # get neignbors of u that are still in Q
+        ng = graph[graph$v1 == u, ]
+        nv = ng$v2
+        nv = nv[nv %in% vertexQ]
+
+        # update path of neighbours
+        for(v in nv){
+          alt = rd[u, ]$dist + ng[ng$v2 == v, ]$w
+          if(alt < rd[v, ]$dist){
+            rd[v, ]$dist = alt
+            rd[v, ]$prev = u
+          }
         }
       }
-    }
-    
-  }
-  return(rd$dist)
 
+    }
+    return(rd$dist)
+  }
 }
