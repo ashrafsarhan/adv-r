6fe07ae508ff70b0bcc3d4c1e22da23b20bf1339
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-09-16 02:21:48 +0200

    Refactoring euclidean unit test to cover euclidean code refactoring

--- ralgo/tests/testthat/test_euclidean.R
+++ ralgo/tests/testthat/test_euclidean.R
@@ -1,28 +1,24 @@
 context('euclidean tests')
 
 test_that("euclidean Character", {
-  expect_that(euclidean(2,"A"))
-  expect_that(euclidean(2,"A"))
+  expect_error(euclidean(2, 'A'), 'args must be numeric!')
+  expect_error(euclidean(2,0.5), 'args must be intergers!')
 })
 
 test_that("euclidean Infinity", {
-  expect_that(euclidean(Inf,Inf))
-  expect_that(euclidean(0,0))
-  expect_that(euclidean(Inf,-Inf))
+  expect_error(euclidean(Inf,Inf))
+  expect_equal(euclidean(0,0), 0)
+  expect_error(euclidean(Inf,-Inf))
 })
 
 test_that("euclidean Logic", {
-  expect_that(euclidean(TRUE,TRUE)) # Returns 1
-  expect_that(euclidean(TRUE,FALSE)) # Error
-  expect_that(euclidean(FALSE,FALSE)) # Returns 0
-})
-
-test_that("euclidean Logic,error", {
   expect_error(euclidean(TRUE,TRUE))
+  expect_error(euclidean(TRUE,FALSE))
+  expect_error(euclidean(FALSE,FALSE))
 })
 
 test_that("euclidean equal", {
-  expect_that(euclidian(200,150), equals(50))
-  expect_that(euclidian(5,20), equals(5))
+  expect_equivalent(euclidean(200,150), 50)
+  expect_equivalent(euclidean(5,20), 5)
 })
 
