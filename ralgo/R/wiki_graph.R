4a28294ea0e4515f202a12e002a57510a473f221
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-09-17 01:26:11 +0200

    Check the graph data file and create .RData-file if it doesn't exist

--- ralgo/R/wiki_graph.R
+++ ralgo/R/wiki_graph.R
@@ -1,6 +1,6 @@
 #' This dataset is used for demonstrating the Dijkstra algorithm,
 #' the three variables representing a graph.
-#' 
+#'
 #' @format A data.frame with 18 rows and three variables:
 #' \describe{
 #'  \item{v1}{Contains edges for the graph}
@@ -16,9 +16,13 @@ wiki_graph <-
              v2=c(2,3,6,1,3,4,1,2,4,6,2,3,5,4,6,1,3,5),
              w=c(7,9,14,7,10,15,9,10,11,2,15,11,6,6,9,14,2,9))
 
-setwd("C:/Github/adv-r/ralgo/data")
-# If you want to re-create this .RData-file, change
-# your workind-directory to your github/reporsitory
+# Check the graph data file and create .RData-file if it doesn't exist
+save_graph_data <- function(file) {
+  if(!file.exists(file)){
+    save(wiki_graph,
+         file = file)
+  }
+}
+graph_data_file <- paste(getwd(), 'data', 'wiki_graph.RData', sep = '/')
+save_graph_data(graph_data_file)
 
-save(wiki_graph,
-     file = "wiki_graph.RData")
