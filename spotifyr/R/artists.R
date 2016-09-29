1e094ffb1ec5c5f091082fbc93239d4addaf726c
Yumeng Li <lemonisa.lee@gmail.com>
2016-09-29 12:38:19 +0200

    add documentation
    add documentation

--- spotifyr/R/artists.R
+++ spotifyr/R/artists.R
@@ -1,6 +1,21 @@
 # You can test this method using the following Frank Sinatra and shawn Mendes spotify artist IDs
 # artistID = '1HBjj22wzbscIZ9sEb5dyf,7n2wHs1TKAczGzO7Dd2rGr'
 
+#' Building a function to access to the specific artist
+#'
+#' This function is building the access to the specific artist
+#'
+#'@param artistID a character of the ID of spotify artist
+#'@param access_token a charater of the access token
+#'@example artistID = '1HBjj22wzbscIZ9sEb5dyf,7n2wHs1TKAczGzO7Dd2rGr'
+#'@return a response
+#'@author Yumeng Li, Mattias Karlsson, Ashraf Sarhan
+#'@details This function is to get the acesss to the artists
+#'@references
+#'\url{https://developer.spotify.com/web-api/authorization-guide/}
+#'@seealso \code{\link{auth}}
+#'@export
+
 getArtists <- function(artistIDs, access_token) {
   HeaderValue = paste0('Bearer ', access_token)
   URI = paste0('https://api.spotify.com/v1/artists?ids=', artistIDs)
