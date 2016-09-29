7b36f131b0f3feaafad7f8873c36361b0624fb39
Mattias Karlsson <tillmattias@gmail.com>
2016-09-29 12:45:03 +0200

    Tests for artists-function. Added documentation

--- spotifyr/R/artists.R
+++ spotifyr/R/artists.R
@@ -1,9 +1,23 @@
 # You can test this method using the following Frank Sinatra and shawn Mendes spotify artist IDs
 # artistID = '1HBjj22wzbscIZ9sEb5dyf,7n2wHs1TKAczGzO7Dd2rGr'
-
+#' Building a function to auth the user
+#'
+#' This function auth the user
+#'
+#'@param artistIDs...
+#'@param access_token
+#'@return a response
+#'@author Yumeng Li, Mattias Karlsson, Ashraf Sarhan
+#'@details Returns information about a specific artists...
+#'@references
+#'\url{https://developer.spotify.com/web-api/authorization-guide/}
+#'@seealso \code{\link{auth}}
+#'@import httr
+#'@export
 getArtists <- function(artistIDs, access_token) {
   HeaderValue = paste0('Bearer ', access_token)
   URI = paste0('https://api.spotify.com/v1/artists?ids=', artistIDs)
   response = GET(url = URI, add_headers(Authorization = HeaderValue))
+  stopifnot(response$status_code == 200)
   return(response)
 }
---
+++ spotifyr/tests/testthat/test_artists.R
@@ -0,0 +1,18 @@
+context('artists tests')
+
+clientID = 'a223e7590d9043f1849bffa7ef86b102'
+secret = 'aa351a255a8c49e7a4d69a8c2f893175'
+artistID = '1HBjj22wzbscIZ9sEb5dyf'
+respons <- auth(clientID,secret) # Extract access_token later on...
+
+test_that("Test valid ID/secret", {
+  #Happy scenario
+  expect_equivalent(artists(artistID,NULL)$status_code, 200)
+})
+
+artistID = 'aa351a255a8c49e7a4d69a8c2f893175###'
+
+test_that("Test with invalid ID/secret", {
+  #Sad scenario
+  expect_error(artists(artistID,NULL)$status_code != 200)
+})
