a6ddf472447a0d6fdec70eac4b9f1e82daf44b66
Mattias Karlsson <tillmattias@gmail.com>
2016-10-07 10:16:09 +0200

    Added documentation and renamed example-code

--- lab6/R/knapsack_dynamic.R
+++ lab6/R/knapsack_dynamic.R
@@ -5,10 +5,10 @@
 #'@param x, data.frame with with two variables weight(w) and value(v)
 #'@param W, the size of knapsack
 #'@examples
-#'brute_force_knapsack(x = knapsack_objects[1:12,], W = 3500)
-#'brute_force_knapsack(x = knapsack_objects[1:8,], W = 2000)
-#'brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
-#'brute_force_knapsack(x = knapsack_objects[1:12,], W = 2000)
+#'knapsack_dynamic(x = knapsack_objects[1:12,], W = 3500)
+#'knapsack_dynamic(x = knapsack_objects[1:8,], W = 2000)
+#'knapsack_dynamick(x = knapsack_objects[1:12,], W = 2000)
+#'knapsack_dynamic(x = knapsack_objects[1:12,], W = 2000)
 #'@references
 #'\url{https://en.wikipedia.org/wiki/Knapsack_problem#0.2F1_knapsack_problem}
 #'@return the maximum knapsack value and which elements
