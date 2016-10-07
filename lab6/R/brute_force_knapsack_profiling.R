28b3b91fe5ac0f8730df85d4d8a311503967b7aa
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-10-07 09:19:26 +0200

    Optimized the brute force search using sum

--- lab6/R/brute_force_knapsack_profiling.R
+++ lab6/R/brute_force_knapsack_profiling.R
@@ -44,3 +44,36 @@ profvis({
   rslt <- list(value=round(maxv), elements=ev)
   print(rslt)
 })
+
+#Optimized code
+profvis({
+  n = nrow(x)  # number of items
+  maxv = 0     # max value
+  ev = c()     # element vector
+  
+  for(i in 0:(2^n-1)){
+    iv = intToBits(i)
+    sv = 0     # sum value
+    sw = 0     # sum weight
+    tv = c()   # temporary value
+    v_vect <- c()
+    w_vect <- c()
+    for(j in 1:n){
+      if(iv[j] == 1){
+        v_vect <- c(v_vect, x[j,'v'])
+        w_vect <- c(w_vect, x[j,'w'])
+        tv <- c(tv, j)
+      }
+    }
+    sv = sum(v_vect)
+    sw = sum(w_vect)
+    
+    if((sv > maxv) && (sw <= W)){
+      maxv = sv
+      ev = tv
+    }
+  }
+  
+  rslt <- list(value=round(maxv), elements=ev)
+  print(rslt)
+})
