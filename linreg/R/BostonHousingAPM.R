65d9bd0a50ee3676ba0505c8f810d981da3655a4
Ashraf Sarhan <ashraf.sar7an@gmail.com>
2016-10-12 02:28:16 +0200

    Divide the BostonHousing data into a test and training dataset using the caret (80%/20% train/test split of the dataset)

---
+++ linreg/R/BostonHousingAPM.R
@@ -0,0 +1,13 @@
+library(caret)
+library(mlbench)
+data(BostonHousing)
+str(BostonHousing)
+set.seed(nrow(BostonHousing))
+
+# define an 80%/20% train/test split of the dataset
+split=0.80
+inTrain <- createDataPartition(BostonHousing$medv, p = split, list = FALSE)
+training <- BostonHousing[inTrain,]
+cat('training: ', nrow(training))
+testing <- BostonHousing[-inTrain,]
+cat('testing: ', nrow(testing))
